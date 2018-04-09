4df3ee1500cc1da7c9889a4d39b7adfdff8f78eb
benjaminhodge <benjamin.andrew.hodge@gmail.com>
2018-04-09 16:20:09 -0500

    adding all servlet get req

--- src/main/java/com/codeup/adlister/controllers/AdsIndexServlet.java
+++ src/main/java/com/codeup/adlister/controllers/AdsIndexServlet.java
@@ -7,10 +7,11 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "IndexServlet", urlPatterns = "/")
-public class IndexServlet extends HttpServlet {
+@WebServlet(name = "AdsIndexServlet" , urlPatterns = "/ads")
+public class AdsIndexServlet extends HttpServlet {
+
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-//        Send the user to the main page
-        request.getRequestDispatcher("index.jsp").forward(request, response);
+//        Serve the adsindex.jsp
+        request.getRequestDispatcher("WEB-INF/ads_index.jsp").forward(request, response);
     }
 }
