7b9c32e636fc07d0b577439eff60f5356052f8a8
zacharyramos <zachary.ramos9@gmail.com>
2018-04-09 15:38:50 -0500

    added parameters to ad and user models

--- src/main/java/com/codeup/adlister/models/Ad.java
+++ src/main/java/com/codeup/adlister/models/Ad.java
@@ -5,12 +5,22 @@ public class Ad {
     private long userId;
     private String title;
     private String description;
+    private double price;
+    private double low_price;
+    private double high_price;
+    private String created_at;
+    private String updated_at;
 
-    public Ad(long id, long userId, String title, String description) {
+    public Ad(long id, long userId, String title, String description, double price, double low_price, double high_price, String created_at, String updated_at) {
         this.id = id;
         this.userId = userId;
         this.title = title;
         this.description = description;
+        this.price = price;
+        this.low_price = low_price;
+        this.high_price = high_price;
+        this.created_at = created_at;
+        this.updated_at = updated_at;
     }
 
     public Ad(long userId, String title, String description) {
--- src/main/java/com/codeup/adlister/models/User.java
+++ src/main/java/com/codeup/adlister/models/User.java
@@ -7,6 +7,12 @@ public class User {
     private String username;
     private String email;
     private String password;
+    private String firstname;
+    private String lastname;
+    private String biograghy;
+    private String profile_pic;
+    private String created_at;
+    private String updated_at;
 
     public User() {}
 
@@ -16,11 +22,17 @@ public class User {
         setPassword(password);
     }
 
-    public User(long id, String username, String email, String password) {
+    public User(long id, String username, String email, String password, String firstname, String lastname, String biograghy, String profile_pic, String created_at, String updated_at) {
         this.id = id;
         this.username = username;
         this.email = email;
         this.password = password;
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.biograghy = biograghy;
+        this.profile_pic = profile_pic;
+        this.created_at = created_at;
+        this.updated_at = updated_at;
     }
 
     public long getId() {
