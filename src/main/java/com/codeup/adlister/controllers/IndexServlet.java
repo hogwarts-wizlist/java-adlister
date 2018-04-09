bc7501e47ef969324bf7a4636339faf6e1b80eca
benjaminhodge <benjamin.andrew.hodge@gmail.com>
2018-04-09 15:30:34 -0500

    added index servlet that routes to home

---
+++ src/main/java/com/codeup/adlister/controllers/IndexServlet.java
@@ -0,0 +1,20 @@
+package com.codeup.adlister.controllers;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "IndexServlet", urlPatterns = "/home")
+public class IndexServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//        Send the user to the main page
+        request.getRequestDispatcher("index.jsp").forward(request, response);
+    }
+}
