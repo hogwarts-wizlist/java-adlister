6e3c6a511b6c24096405c2949cbb3a3c3c074784
benjaminhodge <benjamin.andrew.hodge@gmail.com>
2018-04-10 09:41:51 -0500

    added functionallity to create ad

--- src/main/java/com/codeup/adlister/controllers/AdsIndexServlet.java
+++ src/main/java/com/codeup/adlister/controllers/AdsIndexServlet.java
@@ -1,5 +1,7 @@
 package com.codeup.adlister.controllers;
 
+import com.codeup.adlister.dao.DaoFactory;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -9,8 +11,10 @@ import java.io.IOException;
 
 @WebServlet(name = "AdsIndexServlet" , urlPatterns = "/ads")
 public class AdsIndexServlet extends HttpServlet {
-
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//        Show all ads
+        request.setAttribute("ads", DaoFactory.getAdsDao().all());
+
 //        Serve the adsindex.jsp
         request.getRequestDispatcher("WEB-INF/ads/ads_index.jsp").forward(request, response);
     }
--- src/main/java/com/codeup/adlister/controllers/CreateAdServlet.java
+++ src/main/java/com/codeup/adlister/controllers/CreateAdServlet.java
@@ -1,5 +1,7 @@
 package com.codeup.adlister.controllers;
 
+import com.codeup.adlister.models.Ad;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -9,6 +11,33 @@ import java.io.IOException;
 
 @WebServlet(name = "CreateAdServlet", urlPatterns = "/create")
 public class CreateAdServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//            PULL THE VALUES OF THE NAMES AND CREATE A NEW AD OBJECT
+//                this.id = id;
+//        this.userId = userId;
+//        this.title = title;
+//        this.description = description;
+//        this.price = price;
+//        this.low_price = low_price;
+//        this.high_price = high_price;
+//        this.created_at = created_at;
+//        this.updated_at = updated_at;
+        String title = request.getParameter("title");
+        String description = request.getParameter("description");
+        double price = Double.parseDouble(request.getParameter("price"));
+        double low_price = Double.parseDouble(request.getParameter("low_price"));
+        double high_price = Double.parseDouble(request.getParameter("high_price"));
+        Ad newAd = new Ad(
+                1,
+                1,
+                title,
+                description,
+                price,
+                low_price,
+                high_price
+        );
+    }
+
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 //        Serve CreateAD
         request.getRequestDispatcher("WEB-INF/ads/create_ad.jsp").forward(request, response);
