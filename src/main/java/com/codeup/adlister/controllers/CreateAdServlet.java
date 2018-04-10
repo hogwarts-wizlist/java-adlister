a0b2e877c7ce0f642833aae844b57d5b37e575c0
benjaminhodge <benjamin.andrew.hodge@gmail.com>
2018-04-10 10:48:24 -0500

    added logout servlet

--- src/main/java/com/codeup/adlister/controllers/CreateAdServlet.java
+++ src/main/java/com/codeup/adlister/controllers/CreateAdServlet.java
@@ -13,21 +13,12 @@ import java.io.IOException;
 public class CreateAdServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 //            PULL THE VALUES OF THE NAMES AND CREATE A NEW AD OBJECT
-//                this.id = id;
-//        this.userId = userId;
-//        this.title = title;
-//        this.description = description;
-//        this.price = price;
-//        this.low_price = low_price;
-//        this.high_price = high_price;
-//        this.created_at = created_at;
-//        this.updated_at = updated_at;
         String title = request.getParameter("title");
         String description = request.getParameter("description");
         double price = Double.parseDouble(request.getParameter("price"));
         double low_price = Double.parseDouble(request.getParameter("low_price"));
         double high_price = Double.parseDouble(request.getParameter("high_price"));
-        String ad_image = request.getParameter("high_price");
+        String ad_image = request.getParameter("ad_image");
         Ad newAd = new Ad(
                 1,
                 1,
